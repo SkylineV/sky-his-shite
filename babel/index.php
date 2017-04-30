37a1cacd6fe680aff85c19b6526c4acdff1b627f
Sky <bartek@amusiol.de>
2017-04-30 12:20:43 +0200

    Initial Version

---
+++ babel/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Babel Test</title>
+		<script src="JS/scripts.js" type="text/javascript"></script>
+		<script src="JS/babel.js" type="text/javascript"></script>
+	</head>
+	
+	<body>
+		
+	</body>
+	
+</html>
